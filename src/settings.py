471945fbf69a707af69c79e1281d0356aeec8a6f
peixed351 <peixed351@gmail.com>
2024-02-12 14:21:12 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Windmill Detection on French Aerial"
 PROJECT_NAME_FULL: str = "Windmill Detection on French Aerial Images Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
