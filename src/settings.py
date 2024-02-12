09528b5efffbd3fac723b357d1976da6015fc00d
peixed351 <peixed351@gmail.com>
2024-02-12 19:29:11 +0300

    some check

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,7 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = "Windmill Detection on French Aerial"
+PROJECT_NAME: str = "Windmill Detection on French Aerial Images"
 PROJECT_NAME_FULL: str = "Windmill Detection on French Aerial Images Dataset"
 HIDE_DATASET = False  # set False when 100% sure about repo quality
 
